9c1eccfb154282c12cc2b799684a39dc2a91d634
Guillermo Alvarez <guillermo.alvarez@fiexact.com>
2020-11-01 20:46:19 -0700

    cleanup

--- test.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto')
-
-console.time('1')
-const salt = crypto.randomBytes(64).toString('hex'); 
-const contrasena = crypto.pbkdf2Sync('secret', salt, 10000, 128, 'sha512');
-
-console.timeEnd('1')
-console.log(contrasena.length)
