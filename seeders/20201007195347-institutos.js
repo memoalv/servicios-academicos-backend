2aab14166484a7e7f09c5bbe578b10ddf5b49083
Guillermo Alvarez <guillermo.alvarez@fiexact.com>
2021-04-01 22:23:45 -0600

    add test seeds

--- seeders/20201007195347-institutos.js
+++ seeders/20201007195347-institutos.js
@@ -2,25 +2,36 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+
+    let institutes = [
+      {
+        instituto: "IIT",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        instituto: "IADA",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        instituto: "ICSA",
+        created_at: new Date(),
+        updated_at: new Date(),
+      }
+    ];
+
+    for (let i = 0; i < 100; i++) {
+      institutes.push({
+        instituto: `test-${i}`,
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+    }
+
     await queryInterface.bulkInsert(
       "Institutos",
-      [
-        {
-          instituto: "IIT",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          instituto: "IADA",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          instituto: "ICSA",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-      ],
+      institutes,
       {}
     );
   },
--- seeders/20201007195403-programas.js
+++ seeders/20201007195403-programas.js
@@ -2,20 +2,31 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+
+    const programas =  [
+      {
+        programa: "Ingenieria en sistemas computacionales",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        programa: "Ingenieria fisica",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ];
+
+    for (let i = 0; i < 100; i++) {
+      programas.push({
+        programa: `test-${i}`,
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+    }
+
     await queryInterface.bulkInsert(
       "Programas",
-      [
-        {
-          programa: "Ingenieria en sistemas computacionales",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          programa: "Ingenieria fisica",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-      ],
+      programas,
       {}
     );
   },
