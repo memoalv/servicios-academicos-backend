15fd8561cf2d96f0e684069789765c55141fbd0f
Guillermo Alvarez <guillermo.alvarez@fiexact.com>
2021-04-11 17:54:28 -0600

    fix: migrated campus and programs controllers to generic one

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -21,239 +21,289 @@ router.post(
  * * Rutas referentes a Ventanillas
  *
  */
- const ventanillas = require("../controllers/ventanilla-controller");
- router.post(
-   "/ventanillas/crear",
-   [
-     authMiddleware,
-     ventanillas.validacionListarVentanilla,
-     validReqMiddleware({
-       modulo: "Ventanillas",
-       submodulo: "Listado de ventanillas",
-       accion: "C",
-     }),
-   ],
-   ventanillas.crearVentanilla
- );
- 
- router.get(
-   "/ventanillas/listar",
-   [
-     authMiddleware,
-     ventanillas.validacionListarVentanilla,
-     validReqMiddleware({
-       modulo: "Ventanillas",
-       submodulo: "Listado de ventanillas",
-       accion: "R",
-     }),
-   ],
-   ventanillas.listarVentanilla
- );
- 
- router.patch(
-   "/ventanillas/actualizar",
-   [
-     authMiddleware,
-     ventanillas.validacionListarVentanilla,
-     validReqMiddleware({
-       modulo: "Ventanillas",
-       submodulo: "Listado de ventanillas",
-       accion: "U",
-     }),
-   ],
-   ventanillas.actualizarVentanilla
- );
- 
- router.delete(
-   "/ventanillas/eliminar",
-   [
-     authMiddleware,
-     ventanillas.validacionListarVentanilla,
-     validReqMiddleware({
-       modulo: "Ventanillas",
-       submodulo: "Listado de ventanillas",
-       accion: "D",
-     }),
-   ],
-   ventanillas.borrarVentanilla
- );
+const ventanillas = require("../controllers/ventanilla-controller");
+router.post(
+  "/ventanillas/crear",
+  [
+    authMiddleware,
+    ventanillas.validacionListarVentanilla,
+    validReqMiddleware({
+      modulo: "Ventanillas",
+      submodulo: "Listado de ventanillas",
+      accion: "C",
+    }),
+  ],
+  ventanillas.crearVentanilla
+);
+
+router.get(
+  "/ventanillas/listar",
+  [
+    authMiddleware,
+    ventanillas.validacionListarVentanilla,
+    validReqMiddleware({
+      modulo: "Ventanillas",
+      submodulo: "Listado de ventanillas",
+      accion: "R",
+    }),
+  ],
+  ventanillas.listarVentanilla
+);
+
+router.patch(
+  "/ventanillas/actualizar",
+  [
+    authMiddleware,
+    ventanillas.validacionListarVentanilla,
+    validReqMiddleware({
+      modulo: "Ventanillas",
+      submodulo: "Listado de ventanillas",
+      accion: "U",
+    }),
+  ],
+  ventanillas.actualizarVentanilla
+);
+
+router.delete(
+  "/ventanillas/eliminar",
+  [
+    authMiddleware,
+    ventanillas.validacionListarVentanilla,
+    validReqMiddleware({
+      modulo: "Ventanillas",
+      submodulo: "Listado de ventanillas",
+      accion: "D",
+    }),
+  ],
+  ventanillas.borrarVentanilla
+);
 
 /*************************************
  *
  * * Rutas referentes a PROGRAMAS
  *
  */
-const programas = require("../controllers/programs-controller");
-router.post("/programas/crear", [authMiddleware, programas.validacionCrearPrograma], programas.crearPrograma);
+const crudProgramas = new CrudController("Programa", "programa");
+router.post(
+  "/programas/crear",
+  [
+    authMiddleware,
+    [body("programa").not().isEmpty().trim()],
+    validReqMiddleware({
+      modulo: "Programas",
+      submodulo: "Listado de programas",
+      accion: "C",
+    }),
+  ],
+  crudProgramas.create.bind(crudProgramas)
+);
+router.get(
+  "/programas/listar",
+  [
+    query("pagina").not().isEmpty().isInt(),
+    query("resultados_por_pagina").not().isEmpty().isInt(),
+    query("programa").trim(),
+  ],
+  crudProgramas.read.bind(crudProgramas)
+);
 router.delete(
   "/programas/eliminar",
-  [authMiddleware, programas.validacionEliminarPrograma],
-  programas.eliminarPrograma
+  [
+    authMiddleware,
+    [body("id").not().isEmpty().isInt()],
+    validReqMiddleware({
+      modulo: "Programas",
+      submodulo: "Listado de programas",
+      accion: "D",
+    }),
+  ],
+  crudProgramas.delete.bind(crudProgramas)
 );
 router.patch(
   "/programas/actualizar",
-  [authMiddleware, programas.validacionActualizarPrograma],
-  programas.actualizarPrograma
+  [
+    authMiddleware,
+    [body("id").not().isEmpty().isInt(), body("programa").not().isEmpty().trim()],
+    validReqMiddleware({
+      modulo: "Programas",
+      submodulo: "Listado de programas",
+      accion: "U",
+    }),
+  ],
+  crudProgramas.update.bind(crudProgramas)
 );
-router.get("/programas/listar", [programas.validacionListarProgramas], programas.listarProgramas);
 
 /*************************************
  *
  * * Rutas referentes a INSTITUTOS
  *
  */
-const institutos = require("../controllers/campus-controller");
-router.post("/institutos/crear", [authMiddleware, institutos.validacionCrearInstituto], institutos.crearInstituto);
+const crudInstitutos = new CrudController("Instituto", "instituto");
+router.post(
+  "/institutos/crear",
+  [
+    authMiddleware,
+    [body("instituto").not().isEmpty().trim()],
+    validReqMiddleware({
+      modulo: "Institutos",
+      submodulo: "Listado de institutos",
+      accion: "C",
+    }),
+  ],
+  crudInstitutos.create.bind(crudInstitutos)
+);
+router.get(
+  "/institutos/listar",
+  [
+    query("pagina").not().isEmpty().isInt(),
+    query("resultados_por_pagina").not().isEmpty().isInt(),
+    query("instituto").trim(),
+  ],
+  crudInstitutos.read.bind(crudInstitutos)
+);
 router.delete(
   "/institutos/eliminar",
-  [authMiddleware, institutos.validacionEliminarInstituto],
-  institutos.eliminarInstituto
+  [
+    authMiddleware,
+    [body("id").not().isEmpty().isInt()],
+    validReqMiddleware({
+      modulo: "Institutos",
+      submodulo: "Listado de institutos",
+      accion: "D",
+    }),
+  ],
+  crudInstitutos.delete.bind(crudInstitutos)
 );
 router.patch(
   "/institutos/actualizar",
-  [authMiddleware, institutos.validacionActualizarInstituto],
-  institutos.actualizarInstituto
+  [
+    authMiddleware,
+    [body("id").not().isEmpty().isInt(), body("instituto").not().isEmpty().trim()],
+    validReqMiddleware({
+      modulo: "Institutos",
+      submodulo: "Listado de institutos",
+      accion: "U",
+    }),
+  ],
+  crudInstitutos.update.bind(crudInstitutos)
 );
-router.get("/institutos/listar", [institutos.validacionListarInstitutos], institutos.listarInstitutos);
 
 /*************************************
  *
  * * Rutas referentes a ESCUELAS
  *
  */
- const crudEscuelas = new CrudController("Escuela", "escuela");
- router.post(
-   "/escuelas/crear",
-   [
-     authMiddleware,
-     [body("escuela").isAlphanumeric().not().isEmpty().trim()],
-     validReqMiddleware({
-       modulo: "Escuelas",
-       submodulo: "Listado de escuelas",
-       accion: "C",
-     }),
-   ],
-   crudEscuelas.create.bind(crudEscuelas)
- );
- router.get(
-   "/escuelas/listar",
-   [
-     authMiddleware,
-     [
-       [
-         query("pagina").not().isEmpty().isInt(),
-         query("resultados_por_pagina").not().isEmpty().isInt(),
-         query("escuela").trim(),
-       ],
-     ]
-   ],
-   crudEscuelas.read.bind(crudEscuelas)
- );
- router.patch(
-   "/escuelas/actualizar",
-   [
-     authMiddleware,
-     [body("id").not().isEmpty().isInt(), body("escuela").not().isEmpty().trim()],
-     validReqMiddleware({
-       modulo: "Escuelas",
-       submodulo: "Listado de escuelas",
-       accion: "U",
-     }),
-   ],
-   crudEscuelas.update.bind(crudEscuelas)
- );
- router.delete(
-   "/escuelas/eliminar",
-   [
-     authMiddleware,
-     [body("id").not().isEmpty().isInt()],
-     validReqMiddleware({
-       modulo: "Escuelas",
-       submodulo: "Listado de escuelas",
-       accion: "D",
-     }),
-   ],
-   crudEscuelas.delete.bind(crudEscuelas)
- );
- 
- /*************************************
-  *
-  * * Rutas referentes a DIAS INHABILES
-  *  
-  * Aqui no se actualizarán registros. Solo se listarán, crearán y borrarán.
-  */
- const crudDiasInhabiles = new CrudController("DiasInhabiles", "dia");
- router.post(
-   "/dias_inhabiles/crear",
-   [
-     authMiddleware,
-     [body("dia").not().isEmpty().isInt().isIn([1,2,3,4,5,6,7])],
-     validReqMiddleware(null, "Admin"),
-   ],
-   crudDiasInhabiles.create.bind(crudDiasInhabiles)
- );
- router.get(
-   "/dias_inhabiles/listar",
-   [
-     authMiddleware,
-     [
-       [
-         query("pagina").not().isEmpty().isInt(),
-         query("resultados_por_pagina").not().isEmpty().isInt()
-       ],
-     ],
-     validReqMiddleware(null, "Admin")
-   ],
-   crudDiasInhabiles.read.bind(crudDiasInhabiles)
- );
- router.delete(
-   "/dias_inhabiles/eliminar",
-   [
-     authMiddleware,
-     [body("id").not().isEmpty().isInt()],
-     validReqMiddleware(null, "Admin")
-   ],
-   crudDiasInhabiles.delete.bind(crudDiasInhabiles)
- );
- 
- /*************************************
-  *
-  * * Rutas referentes a FECHAS INHABILES
-  *  
-  *  Aqui no se actualizarán registros. Solo se listarán, crearán y borrarán.
-  */
- const crudFechasInhabiles = new CrudController("FechasInhabiles", "fecha");
- router.post(
-   "/fechas_inhabiles/crear",
-   [
-     authMiddleware,
-     [body("fecha").not().isEmpty().isDate()],
-     validReqMiddleware(null, "Admin"),
-   ],
-   crudFechasInhabiles.create.bind(crudFechasInhabiles)
- );
- router.get(
-   "/fechas_inhabiles/listar",
-   [
-     authMiddleware,
-     [
-       [
-         query("pagina").not().isEmpty().isInt(),
-         query("resultados_por_pagina").not().isEmpty().isInt()
-       ],
-     ],
-     validReqMiddleware(null, "Admin")
-   ],
-   crudFechasInhabiles.read.bind(crudFechasInhabiles)
- );
- router.delete(
-   "/fechas_inhabiles/eliminar",
-   [
-     authMiddleware,
-     [body("id").not().isEmpty().isInt()],
-     validReqMiddleware(null, "Admin")
-   ],
-   crudFechasInhabiles.delete.bind(crudFechasInhabiles)
- );
+const crudEscuelas = new CrudController("Escuela", "escuela");
+router.post(
+  "/escuelas/crear",
+  [
+    authMiddleware,
+    [body("escuela").isAlphanumeric().not().isEmpty().trim()],
+    validReqMiddleware({
+      modulo: "Escuelas",
+      submodulo: "Listado de escuelas",
+      accion: "C",
+    }),
+  ],
+  crudEscuelas.create.bind(crudEscuelas)
+);
+router.get(
+  "/escuelas/listar",
+  [
+    authMiddleware,
+    [
+      [
+        query("pagina").not().isEmpty().isInt(),
+        query("resultados_por_pagina").not().isEmpty().isInt(),
+        query("escuela").trim(),
+      ],
+    ],
+  ],
+  crudEscuelas.read.bind(crudEscuelas)
+);
+router.patch(
+  "/escuelas/actualizar",
+  [
+    authMiddleware,
+    [body("id").not().isEmpty().isInt(), body("escuela").not().isEmpty().trim()],
+    validReqMiddleware({
+      modulo: "Escuelas",
+      submodulo: "Listado de escuelas",
+      accion: "U",
+    }),
+  ],
+  crudEscuelas.update.bind(crudEscuelas)
+);
+router.delete(
+  "/escuelas/eliminar",
+  [
+    authMiddleware,
+    [body("id").not().isEmpty().isInt()],
+    validReqMiddleware({
+      modulo: "Escuelas",
+      submodulo: "Listado de escuelas",
+      accion: "D",
+    }),
+  ],
+  crudEscuelas.delete.bind(crudEscuelas)
+);
+
+/*************************************
+ *
+ * * Rutas referentes a DIAS INHABILES
+ *
+ * Aqui no se actualizarán registros. Solo se listarán, crearán y borrarán.
+ */
+const crudDiasInhabiles = new CrudController("DiasInhabiles", "dia");
+router.post(
+  "/dias_inhabiles/crear",
+  [
+    authMiddleware,
+    [body("dia").not().isEmpty().isInt().isIn([1, 2, 3, 4, 5, 6, 7])],
+    validReqMiddleware(null, "Admin"),
+  ],
+  crudDiasInhabiles.create.bind(crudDiasInhabiles)
+);
+router.get(
+  "/dias_inhabiles/listar",
+  [
+    authMiddleware,
+    [[query("pagina").not().isEmpty().isInt(), query("resultados_por_pagina").not().isEmpty().isInt()]],
+    validReqMiddleware(null, "Admin"),
+  ],
+  crudDiasInhabiles.read.bind(crudDiasInhabiles)
+);
+router.delete(
+  "/dias_inhabiles/eliminar",
+  [authMiddleware, [body("id").not().isEmpty().isInt()], validReqMiddleware(null, "Admin")],
+  crudDiasInhabiles.delete.bind(crudDiasInhabiles)
+);
+
+/*************************************
+ *
+ * * Rutas referentes a FECHAS INHABILES
+ *
+ *  Aqui no se actualizarán registros. Solo se listarán, crearán y borrarán.
+ */
+const crudFechasInhabiles = new CrudController("FechasInhabiles", "fecha");
+router.post(
+  "/fechas_inhabiles/crear",
+  [authMiddleware, [body("fecha").not().isEmpty().isDate()], validReqMiddleware(null, "Admin")],
+  crudFechasInhabiles.create.bind(crudFechasInhabiles)
+);
+router.get(
+  "/fechas_inhabiles/listar",
+  [
+    authMiddleware,
+    [[query("pagina").not().isEmpty().isInt(), query("resultados_por_pagina").not().isEmpty().isInt()]],
+    validReqMiddleware(null, "Admin"),
+  ],
+  crudFechasInhabiles.read.bind(crudFechasInhabiles)
+);
+router.delete(
+  "/fechas_inhabiles/eliminar",
+  [authMiddleware, [body("id").not().isEmpty().isInt()], validReqMiddleware(null, "Admin")],
+  crudFechasInhabiles.delete.bind(crudFechasInhabiles)
+);
 
 module.exports = router;
